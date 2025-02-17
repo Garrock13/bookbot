2f70d37d370c317c896f54fa50d2cf4cfcc991fc
Gilles ROURE <gilles.roure@apec.fr>
2025-02-17 14:58:19 +0100

    print report of containing book

---
+++ main.py
@@ -0,0 +1,45 @@
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    num_chars = get_num_chars(text)
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{num_words} words found in the document\n")
+    print_chars_report(convert_dict_to_list_with_only_alpha(num_chars))
+    print(f"--- End report ---")
+
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+def get_num_chars(text):
+    chars = {}
+    for c in text.lower():
+        if c in chars:
+            chars[c] = chars[c] + 1
+        else:
+           chars[c] = 1
+    return chars
+
+def convert_dict_to_list_with_only_alpha(dict):
+    list = []
+    for key in dict:
+        if key.isalpha():
+            list.append({"name": key, "num": dict[key]})
+    list.sort(reverse=True, key=sort_by_num)
+    return list
+
+def sort_by_num(dict):
+    return dict["num"]
+
+def print_chars_report(list):
+    for dict in list:
+        name = dict["name"]
+        num = dict["num"]
+        print(f"the '{name}' character was found {num} times")
+
+main()
